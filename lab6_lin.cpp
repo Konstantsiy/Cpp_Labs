58405280b1bc2d7cfee82dff6949ae222d115798
Konstantsiy <62259384+Konstantsiy@users.noreply.github.com>
2020-05-08 17:46:09 +0300

    Update lab6_lin.cpp

--- lab6_lin.cpp
+++ lab6_lin.cpp
@@ -1,4 +1,4 @@
-#include <string.h>
+#include <stdio.h>
 #include <iostream>
 
 using namespace std;
@@ -6,168 +6,124 @@ using namespace std;
 const int MAX_POOLS = 1000;
 const long BUF_SIZE = 104888320;
 
-static char ALLBUF[BUF_SIZE];             // доступная память
-static unsigned long AVAIBLE = BUF_SIZE;  // свободно
+static char ALLBUF[BUF_SIZE];
+static unsigned long AVAIBLE = BUF_SIZE;
+static char* pools[MAX_POOLS];
+static unsigned int POOLS_COUNT = 1;
+static unsigned int pools_size[MAX_POOLS];
 
-static char* pools[MAX_POOLS];              // пулы
+static char* blocks[MAX_POOLS];
+static unsigned int BLOCKS_COUNT = 0;
+static unsigned int block_size[MAX_POOLS];
+static unsigned int RIGHT_BLOCK;
 
-static unsigned int POOLS_COUNT = 1;        // количество пулов
-
-static unsigned int pools_size[MAX_POOLS]; // размеры пулов
-
-static char* blocks[MAX_POOLS];  // блоки
-static unsigned int BLOCKS_COUNT = 0;  // количетсво существующих блоков
-static unsigned int block_size[MAX_POOLS]; // размеры блоков
-static unsigned int RIGHT_BLOCK;  // номер самого правого блока
-
-#define NO_MEMORY 1
-#define BLOCK_NOT_FOUND 2
+#define BLOCK_NOT_FOUND -1
 
 
-void ars_alloc_init(void);   // инициализация
-char* ars_malloc(unsigned long Size);  // выделение
-int ars_free(char*);                  // освобождение
-int ars_defrag(void);               // дефрагментация
-void ars_alloc_stat();   // статистика
 
+char* __malloc(unsigned long Size) {
+    unsigned long int i, k;
+    char* p;
+
+    if (Size > AVAIBLE) {
+        return nullptr;
+    }
+
+    p = 0;
+
+    for (i = 0; i < POOLS_COUNT; ++i)
+        if (Size <= pools_size[i]) {
+            p = pools[i];
+            k = i;
+            break;
+        }
+
+    if (!p) {
+        return nullptr;
+    }
+
+    blocks[BLOCKS_COUNT] = p;
+    block_size[BLOCKS_COUNT] = Size;
+    ++BLOCKS_COUNT;
+    ++RIGHT_BLOCK;
+    pools[k] = (char*)(p + Size + 1);
+    pools_size[k] = pools_size[k] - Size;
+
+    AVAIBLE -= Size;
+    return p;
+}
+
+int __free(char* block) {
+    unsigned int i, k;
+    char* p = 0;
+    for (i = 0; i < RIGHT_BLOCK; ++i)
+        if (block == blocks[i])
+        {
+            p = blocks[i];
+            k = i;
+            break;
+        }
+    if (!p) {
+        return BLOCK_NOT_FOUND;
+    }
+
+    blocks[k] = 0;
+    --BLOCKS_COUNT;
+    pools[POOLS_COUNT] = block;
+    pools_size[POOLS_COUNT] = block_size[k];
+    ++POOLS_COUNT;
+    AVAIBLE += block_size[k];
+    return 0;
+}
+
+int __defrag(void) {
+    unsigned int i, k;
+    char* p = ALLBUF;
+    char* t, * tmp;
+
+    for (i = 0; i < RIGHT_BLOCK; ++i) {
+        t = blocks[i];
+        if (t == ALLBUF) {
+            p = (char*)(blocks[i] + block_size[i] + 1);
+            continue;
+        }
+        tmp = p;
+        for (k = 0, t = blocks[i]; k < block_size[i]; ++k)
+            *p++ = *t++;
+        blocks[i] = tmp;
+    }
+
+    POOLS_COUNT = 1;
+    pools[0] = p;
+    AVAIBLE = BUF_SIZE - (unsigned long)(p - ALLBUF);
+    pools_size[0] = AVAIBLE;
+    RIGHT_BLOCK = 0;
+    return 0;
+}
+
+void getData() {
+    cout << "\tAvailable memory: "<< AVAIBLE << "of "<< BUF_SIZE << " bytes\n\tCount of blocks: " << BLOCKS_COUNT << endl << endl;
+}
 
 int main() {
-	ars_alloc_init();
-	bool flag = true;
-	char* block = nullptr;
-	cout << "1. malloc.\n2. free.\n3. defrag.\n4. get statistics.\n0. End.\n";
-	while (flag) {
-		cout << ">";
-		switch (getchar()) {
-		case '1': {
-			int memorySize;
-			cout << "Enter size of needed memory: ";
-			cin >> memorySize;
-			block = ars_malloc(memorySize);
-			if (block == nullptr) {
-				cout << "Error allocaction memory.\n";
-				return -1;
-			}
-			break;
-		}
-		case '2': {
-			ars_free(block);
-			break;
-		}
-		case '3': {
-			ars_defrag();
-			break;
-		}
-		case '4': {
-			ars_alloc_stat();
-			break;
-		}
-		case '0': 
-			flag = false;
-			break;
-		}
-	}
-	return 0;
-}
 
-//=========================================
-void ars_alloc_init(void) {
-	pools[0] = ALLBUF; // начало первого пула
-	pools_size[0] = AVAIBLE;
-}
-//=========================================
+    pools[0] = ALLBUF;
+    pools_size[0] = AVAIBLE;
 
-char* ars_malloc(unsigned long Size) {
-	unsigned long int i, k;
-	char *p;
+    char* ptr0 = __malloc(11);
+    getData();
+    __free(ptr0);
+    char* ptr2 = __malloc(12);
+    getData();
+    __free(ptr2);
+    char* ptr4 = __malloc(9);
+    getData();
+    char* ptr5 = __malloc(7);
+    getData();
 
-	// если требуют больше, чем есть
-	if (Size > AVAIBLE) {
-		return nullptr;
-	}
-
-	p = 0;
-	// ищем первый подходящий пул
-	for (i = 0; i < POOLS_COUNT; ++i)
-		if (Size <= pools_size[i]) {
-			p = pools[i]; // запоминаем адрес
-			k = i;        // и номер
-			break;
-		}
-
-	if (!p) {
-		return nullptr;
-	}
-
-	blocks[BLOCKS_COUNT] = p;  // здесь будет новый блок
-	block_size[BLOCKS_COUNT] = Size;
-	++BLOCKS_COUNT;
-	++RIGHT_BLOCK;
-	pools[k] = (char*)(p + Size + 1);  // смещаем адрес начала пула на конец блока
-	pools_size[k] = pools_size[k] - Size;  // и исправляем размер
-
-	AVAIBLE -= Size;  // места стало меньше
-	return p;
-}
-//==========================================
-
-int ars_free(char* block)
-{
-	unsigned int i, k;
-	char* p = 0;
-	// ищем блок по адресу
-	for (i = 0; i < RIGHT_BLOCK; ++i)
-		if (block == blocks[i]) {
-			p = blocks[i];
-			k = i;
-			break;
-		}
-	if (!p) {
-		return BLOCK_NOT_FOUND;  // блок не найден
-	}
-
-	blocks[k] = 0;
-	--BLOCKS_COUNT;
-	pools[POOLS_COUNT] = block; // превращаем блок в пул
-	pools_size[POOLS_COUNT] = block_size[k];
-	++POOLS_COUNT;
-	AVAIBLE += block_size[k]; // места стало больше
-
-	return 0;
-}
-//==========================================
-
-// функция дефрагментирования. Смещает все блоки влево и оставляет
-//  единственный пул
-
-int ars_defrag(void) {
-	unsigned int i, k;
-	char* p = ALLBUF;
-	char* t, *tmp;
-
-	for (i = 0; i < RIGHT_BLOCK; ++i)
-	{
-		t = blocks[i];
-		if (t == ALLBUF) {
-			p = (char*)(blocks[i] + block_size[i] + 1);
-			continue;
-		}
-		tmp = p;
-		for (k = 0, t = blocks[i]; k < block_size[i]; ++k) *p++ = *t++;
-		blocks[i] = tmp;
-	}
-
-	POOLS_COUNT = 1;
-	BLOCKS_COUNT = POOLS_COUNT;
-	pools[0] = p;
-	AVAIBLE = BUF_SIZE - (unsigned long)(p - ALLBUF + 1);
-	pools_size[0] = AVAIBLE;
-	RIGHT_BLOCK = 0;
-	return 0;
-}
-//==========================
-
-// вывод статистики
-void ars_alloc_stat() {
-	printf("\tAvaible memory: %ld of %ld bytes\n\tCount of blocks: %d\n", AVAIBLE, BUF_SIZE, BLOCKS_COUNT);
+    __defrag();
+    cout << "\nAfter defragmentation.\n";
+    getData();
+    getchar();
+    return 0;
 }
